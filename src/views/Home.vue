80b68ad44bbba02ee411935ec2859b4016ce99d2
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-10-18 21:30:38 -0500

    vue frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+
+    <div>
+      Name: <input v-model="newPlace.name">
+      Address: <input v-model="newPlace.address">
+      <button v-on:click="addPlace()">Add Place</button>
+    </div>
+
+    <div v-for="place in places"> {{places}}</div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+  var axios = require('axios');
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      places: [],
+      newPlace: {name: "", address:""},
+      errors: []
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/places")
+    .then(response => {
+      this.places = response.data;
+    });
+  },
+  methods: {
+    addPlace: function() {
+      this.errors = [];
+      var params = {
+                    name: this.newPlace.name,
+                    address: this.newPlace.address
+                    };
+      axios
+      .post("http://localhost:3000/api/places", params)
+      .then(response => {
+        this.places.push(response.data);
+        this.newPlace = {name: "", address: ""};
+      })
+      .catch(error => {
+        this.errors = error.response.data.errors;
+      }
+      });
+  },
+  computed: {}
+};
 </script>
